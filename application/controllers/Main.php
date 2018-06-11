fcd3c65c7903d332506cf008b7449169fcabce43
ip-d <monu_kanyal@esferasoft.com>
2018-06-11 10:35:12 +0530

    second init

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -3,12 +3,13 @@ class Main extends CI_Controller
 {
 	public function index()
 	{
-		$this->load->help('url');
+		$this->load->helper('url');
 	}
 	
 	public function logout()
 	{
 		$this->session->sess_destroy();
+		redirect('/login');
 	}
 }
 ?>
